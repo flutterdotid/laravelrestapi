75a39d419a55d2c1be2edfaa25d5041a31fc832e
Ilham Maulana <k4ilham@gmail.com>
2020-03-06 19:10:23 +0700

    komit1
    komit1

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class UserResource extends Resource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'email' => $this->email,
+        ];
+    }
+}
